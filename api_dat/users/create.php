ed7df8a92295c2e92f4febc4f04569e860ceabf6
QuocDat <dathoa1104@gmail.com>
2024-10-18 23:14:16 +0700

    abc

--- api_dat/users/create.php
+++ api_dat/users/create.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: *"); // Cho phép tất cả các miền
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); // Cho phép các phương thức
+header("Access-Control-Allow-Headers: Content-Type, Authorization"); // Cho phép các header
+
 header('Content-Type: application/json'); // Đặt loại nội dung cho phản hồi
 
 require_once '../../source/models/UserModel.php'; // Nhúng model
