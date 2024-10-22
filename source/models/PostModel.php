078097a65f6b3b8bb66ed4afef1f3f36f4a643f0
QuocDat <dathoa1104@gmail.com>
2024-10-22 14:02:54 +0700

    update

--- source/models/PostModel.php
+++ source/models/PostModel.php
@@ -60,10 +60,16 @@ class Post extends BaseModel
     public function getPostsByUserId($userId)
     {
         // $sql = "SELECT * FROM `posts` INNER JOIN users ON users.id = posts.user_id   WHERE user_id = :id";
-        $sql = "SELECT *, posts.id,  COUNT(post_like.post_id) AS like_count
+        $sql = "SELECT  posts.id,  posts.content, users.name, posts.created_at,  media.url,
+COUNT(post_like.post_id) AS like_count, 
+COUNT(post_comments.post_id) AS
+cmt_count,COUNT(post_share.post_id) AS share_count
         FROM posts
         INNER JOIN users ON users.id = posts.user_id
         LEFT JOIN post_like ON post_like.post_id = posts.id
+        LEFT JOIN post_comments ON post_comments.post_id = posts.id
+        LEFT JOIN post_share ON post_share.post_id = posts.id
+        LEFT JOIN media ON media.post_id = posts.id AND media.is_avatar = 0
         WHERE posts.user_id = :id
         GROUP BY posts.id";
 
@@ -72,12 +78,4 @@ class Post extends BaseModel
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-
-    public function getIdByUserId($userId)
-    {
-        $sql = "SELECT posts.id, COUNT(user_like_id) AS total_likes FROM posts INNER JOIN post_like ON post_like.post_id = posts.id WHERE posts.id = :id";
-        $stmt = $this->conn->prepare($sql);
-        $stmt->execute([':id' => $userId]);
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
 }
