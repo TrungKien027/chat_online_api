2b298c11c753d5069e7bc705ea767066c542cbd5
QuocDat <dathoa1104@gmail.com>
2024-10-24 13:01:34 +0700

    update api

--- source/models/PostModel.php
+++ source/models/PostModel.php
@@ -52,18 +52,33 @@ class Post extends BaseModel
     public function getPostsByUserId($userId)
     {
         // $sql = "SELECT * FROM `posts` INNER JOIN users ON users.id = posts.user_id   WHERE user_id = :id";
-        $sql = "SELECT  posts.id,  posts.content, users.name, posts.created_at,  media.url,
-COUNT(post_like.post_id) AS like_count, 
-COUNT(post_comments.post_id) AS
-cmt_count,COUNT(post_share.post_id) AS share_count
-        FROM posts
-        INNER JOIN users ON users.id = posts.user_id
-        LEFT JOIN post_like ON post_like.post_id = posts.id
-        LEFT JOIN post_comments ON post_comments.post_id = posts.id
-        LEFT JOIN post_share ON post_share.post_id = posts.id
-        LEFT JOIN media ON media.post_id = posts.id AND media.is_avatar = 0
-        WHERE posts.user_id = :id
-        GROUP BY posts.id";
+        $sql = "(SELECT posts.id, posts.content, users.name, posts.created_at, media.url,
+        COUNT(post_like.post_id) AS like_count, 
+        COUNT(post_comments.post_id) AS cmt_count,
+        COUNT(post_share.post_id) AS share_count
+ FROM posts
+ INNER JOIN users ON users.id = posts.user_id
+ LEFT JOIN post_like ON post_like.post_id = posts.id
+ LEFT JOIN post_comments ON post_comments.post_id = posts.id
+ LEFT JOIN post_share ON post_share.post_id = posts.id
+ LEFT JOIN media ON media.post_id = posts.id AND media.is_avatar = 0
+ WHERE posts.user_id = :id
+ GROUP BY posts.id)
+
+UNION
+
+(SELECT original_posts.id, original_posts.content, original_users.name, original_posts.created_at, media.url,
+        COUNT(post_like.post_id) AS like_count, 
+        COUNT(post_comments.post_id) AS cmt_count,
+        COUNT(post_share.post_id) AS share_count
+ FROM post_share
+ INNER JOIN posts AS original_posts ON post_share.post_id = original_posts.id
+ INNER JOIN users AS original_users ON original_users.id = original_posts.user_id
+ LEFT JOIN post_like ON post_like.post_id = original_posts.id
+ LEFT JOIN post_comments ON post_comments.post_id = original_posts.id
+ LEFT JOIN media ON media.post_id = original_posts.id AND media.is_avatar = 0
+ WHERE post_share.user_share_id = :id
+ GROUP BY original_posts.id)";
 
         $stmt = $this->conn->prepare($sql);
         $stmt->execute([':id' => $userId]);
